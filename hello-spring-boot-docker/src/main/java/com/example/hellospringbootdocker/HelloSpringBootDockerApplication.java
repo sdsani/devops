99b6e929a725f7fd1e93696eca8708d6efbc7627
sdsani <sds.78692@gmail.com>
2022-04-16 08:53:33 -0500

    first commit

---
+++ hello-spring-boot-docker/src/main/java/com/example/hellospringbootdocker/HelloSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ hello-spring-boot-docker/src/test/java/com/example/hellospringbootdocker/HelloSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
