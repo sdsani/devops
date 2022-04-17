3b092949ea3f93d977f3777a81b1a9c970e71658
sdsani <sds.78692@gmail.com>
2022-04-17 14:33:55 -0500

    Added more steps to workflow

---
+++ hello-spring-boot-docker/src/test/java/com/example/hellospringbootdocker/Junit5Test.java
@@ -0,0 +1,11 @@
+package com.example.hellospringbootdocker;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5Test {
+
+	@Test
+	void testHelloMessage() {
+		System.err.println("Message from testHelloMessage");
+	}
+}
